489a61e1068d27182c56122bc2417dea1421f218
Jeffrey Scott <valenpendragon@gmail.com>
2023-07-04 13:20:34 -0400

    Implement form submission and verify proper data has been sent.

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    print(f"request method: {request.method}")
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        print(f"first name: {first_name}")
+        last_name = request.form["last_name"]
+        print(f"last name: {last_name}")
+        email = request.form["email"]
+        print(f"email: {email}")
+        start_date = request.form["start_date"]
+        print(f"start date: {start_date}")
+        occupation = request.form["occupation"]
+        print(f"occupation: {occupation}")
     return render_template("index.html")
 
 
