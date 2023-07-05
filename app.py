1873e6cbee1d6d62eb52165db79eb2b81f749bd9
Jeffrey Scott <valenpendragon@gmail.com>
2023-07-05 15:56:22 -0400

    Add sending of confirmation email to new user.

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
 import os
 from datetime import datetime
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("FLASK_JOB_FORM")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = os.getenv("GMAIL_ACCOUNT")
+app.config["MAIL_PASSWORD"] = os.getenv("GMAIL_APP_PASSWORD")
 db = SQLAlchemy(app)
+
+mail = Mail(app)
 
 
 class Form(db.Model):
@@ -39,6 +47,20 @@ def index():
                     occupation=occupation)
         db.session.add(form)
         db.session.commit()
+
+        message_body = f"Thank you for your submission, {first_name}. " \
+                       f"Here is the information you submitted to us : \n" \
+                       f"{first_name} {last_name}\n " \
+                       f"Employment Status: {occupation}\n " \
+                       f"Date available: {date} \n" \
+                       f"Thank you again."
+
+        message = Message(subject="New form submission",
+                          sender=app.config["MAIL_USERNAME"],
+                          recipients=[email],
+                          body=message_body)
+        mail.send(message)
+
         flash(f"{first_name}, your form was submitted successfully!", "success")
 
     return render_template("index.html")
